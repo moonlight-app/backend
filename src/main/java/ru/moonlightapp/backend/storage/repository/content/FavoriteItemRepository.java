55cb63fc7b10a40d798b3f09aab89b0722efecbd
SoKnight <mcliyanage2018@gmail.com>
2024-11-06 23:48:03 +0500

    Remove useless repository method

--- src/main/java/ru/moonlightapp/backend/storage/repository/content/FavoriteItemRepository.java
+++ src/main/java/ru/moonlightapp/backend/storage/repository/content/FavoriteItemRepository.java
@@ -4,14 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.moonlightapp.backend.storage.model.content.FavoriteItem;
 
-import java.util.Optional;
 import java.util.Set;
 
 public interface FavoriteItemRepository extends JpaRepository<FavoriteItem, Long> {
 
     boolean existsByOwnerEmailAndProductId(String ownerEmail, long productId);
-
-    Optional<FavoriteItem> findByOwnerEmailAndProductId(String ownerEmail, long productId);
 
     @Query("select i.product.id from FavoriteItem i where i.owner.email = ?1 and i.product.id in ?2")
     Set<Integer> keepOnlyFavoriteProductIds(String userEmail, int[] productIds);
