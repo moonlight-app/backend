5786cc20a821c65f3337f86347804eea998826a2
SoKnight <mcliyanage2018@gmail.com>
2024-11-06 09:41:44 +0500

    Add favorite products IDs searching query

--- src/main/java/ru/moonlightapp/backend/storage/repository/content/FavoriteProductRepository.java
+++ src/main/java/ru/moonlightapp/backend/storage/repository/content/FavoriteProductRepository.java
@@ -1,8 +1,14 @@
 package ru.moonlightapp.backend.storage.repository.content;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.moonlightapp.backend.storage.model.content.FavoriteProduct;
 
+import java.util.Set;
+
 public interface FavoriteProductRepository extends JpaRepository<FavoriteProduct, Long> {
+
+    @Query("select p.product.id from FavoriteProduct p where p.owner.email = ?1")
+    Set<Long> findFavoriteProductIds(String userEmail);
 
 }
