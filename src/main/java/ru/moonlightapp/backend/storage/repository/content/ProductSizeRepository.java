a365380e10ea9ebfe95f61f414e8e3a7388292bc
SoKnight <mcliyanage2018@gmail.com>
2024-11-17 04:16:28 +0500

    Remove useless custom query

--- src/main/java/ru/moonlightapp/backend/storage/repository/content/ProductSizeRepository.java
+++ src/main/java/ru/moonlightapp/backend/storage/repository/content/ProductSizeRepository.java
@@ -1,19 +1,8 @@
 package ru.moonlightapp.backend.storage.repository.content;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import ru.moonlightapp.backend.storage.model.content.ProductSize;
 
 public interface ProductSizeRepository extends JpaRepository<ProductSize, Long> {
-
-    @Query(value = """
-            select s.size from product_sizes s
-            join product_size_mappings m on m.product_size_id = s.id
-            where s.product_types & ?1 <> 0
-            group by s.size
-            order by count(*) desc
-            limit ?2
-            """, nativeQuery = true)
-    float[] findPopularSizes(int productTypeBit, int limit);
 
 }
