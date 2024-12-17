1432e3cc1e209904048020179089a6a023dbf619
SoKnight <mcliyanage2018@gmail.com>
2024-12-17 11:51:11 +0500

    Register all web converters

--- src/main/java/ru/moonlightapp/backend/config/WebMvcConfig.java
+++ src/main/java/ru/moonlightapp/backend/config/WebMvcConfig.java
@@ -1,16 +1,26 @@
 package ru.moonlightapp.backend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.moonlightapp.backend.web.converter.CatalogSortingConverter;
+import ru.moonlightapp.backend.web.converter.ProductTypeConverter;
 import ru.moonlightapp.backend.web.converter.SexConverter;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final CatalogSortingConverter catalogSortingConverter;
+    private final ProductTypeConverter productTypeConverter;
+    private final SexConverter sexConverter;
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new SexConverter());
+        registry.addConverter(catalogSortingConverter);
+        registry.addConverter(productTypeConverter);
+        registry.addConverter(sexConverter);
     }
 
 }
